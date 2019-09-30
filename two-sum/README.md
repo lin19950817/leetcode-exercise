7d02f29654423b2ad3e10ec1014723a365ffe597
LinZhenNan <598296448@qq.com>
2019-09-30 10:17:30 +0800

    [IMP]更新规范

--- two-sum/README.md
+++ two-sum/README.md
@@ -3,9 +3,12 @@
 
 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
 ### 示例:
+```
 给定 nums = [2, 7, 11, 15], target = 9
 
-因为 nums[0] + nums[1] = 2 + 7 = 9, 所以返回 [0, 1]
+因为 nums[0] + nums[1] = 2 + 7 = 9, 
+所以返回 [0, 1]
+```
 
 ***
 # Two Sum
@@ -13,7 +16,9 @@ Given an array of integers, return **indices** of the two numbers such that they
 
 You may assume that each input would have **exactly** one solution, and you may not use the same element twice.
 ### Example:
+```
 Given nums = [2, 7, 11, 15], target = 9,
 
 Because nums[0] + nums[1] = 2 + 7 = 9,
 return [0, 1].
+```
--- two-sum/src/com/salamence/TwoSum.java
+++ two-sum/src/com/salamence/TwoSum.java
@@ -11,35 +11,33 @@ import java.util.Map;
 public class TwoSum {
 
     /**
-     * 两数之和
-     *  遍历 nums并分别与计算与 target的差 minus，使用 map记录，
-     *  key为 num，value为 num下标。每次遍历计算差时查看 map(key = minus)
-     *  中是否有记录,如果有则返回结果，没有则将 key = num记录进 map
+     * 两数之和 3ms
+     * 遍历 nums并分别与计算与 target的差 minus，使用 map记录，
+     * key为 num，value为 num下标。每次遍历计算差时查看 map(key = minus)
+     * 中是否有记录,如果有则返回结果，没有则将 key = num记录进 map
      *
-     * @param nums
-     * @param target
-     * @author LinZhenNan 598296448@qq.com 2019-09-29 23:35
+     * @param nums   整数数组
+     * @param target 目标值
      * @return int[]
+     * @author LinZhenNan lin_hehe@qq.com 2019-09-29 23:35
      */
     public int[] twoSum(int[] nums, int target) {
         // 用来记录，key为 num，value为 num下标
-        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        Map<Integer, Integer> map = new HashMap<>(16);
         for (int i = 0; i < nums.length; i++) {
             // 计算目标值与 num的差
             int minus = target - nums[i];
             // 取出记录中 key为目标值的下标
             Integer sub = map.get(minus);
             if (null == sub) {
-                if (null == map.get(nums[i])) {
-                    // 如果目标值和 num都没有记录则将当前 num记录
-                    map.put(nums[i], i);
-                }
+                // 如果目标值和 num都没有记录则将当前 num记录
+                map.putIfAbsent(nums[i], i);
             } else {
                 // 在记录中找到目标值的下标则返回结果
                 return new int[]{sub, i};
             }
         }
         // 遍历 nums都没有找到和为目标值的数组
-        return null;
+        throw new IllegalArgumentException("No two sum value");
     }
 }
