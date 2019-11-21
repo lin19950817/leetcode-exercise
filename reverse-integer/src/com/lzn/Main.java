e25deaede610edf1509eee6c0cd76a426b695447
LinZhenNan <598296448@qq.com>
2019-11-21 22:42:52 +0800

    [IMP]reverseInteger完成

--- reverse-integer/src/com/lzn/Main.java
+++ reverse-integer/src/com/lzn/Main.java
@@ -1,24 +1,39 @@
 package com.lzn;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 入口
  *
  * @author LinZhenNan lin_hehe@qq.com 2019/10/24 23:06
  */
 public class Main {
+    /**
+     * case, key 实例, value 结果
+     */
+    public static Map<Integer, Integer> c;
+
+    static {
+        c = new HashMap<>(16);
+        c.put(0, 0);
+        c.put(2147483647, 0);
+        c.put(-2147483648, 0);
+        c.put(-2147483641, -1463847412);
+        c.put(1234567000, 7654321);
+    }
 
     public static void main(String[] args) {
-        int i1 = 0;
-        int i2 = 2147483647;
-        int i3 = -2147483648;
-        int i4 = -2147483641;
-        int i5 = 1234567000;
+        ReverseInteger r = new ReverseInteger();
+        for (Integer item :
+                c.keySet()) {
+            isOk(item, r.reverse(item), c.get(item));
+        }
+    }
 
-        ReverseInteger reverseInteger = new ReverseInteger();
-        System.out.println(reverseInteger.reverse(i1));
-        System.out.println(reverseInteger.reverse(i2));
-        System.out.println(reverseInteger.reverse(i3));
-        System.out.println(reverseInteger.reverse(i4));
-        System.out.println(reverseInteger.reverse(i5));
+    public static void isOk(int input, int output, int answer) {
+        if (output != answer) {
+            throw new RuntimeException("input : " + input + "\t output : " + output + "\t answer" + answer);
+        }
     }
 }
--- reverse-integer/src/com/lzn/ReverseInteger.java
+++ reverse-integer/src/com/lzn/ReverseInteger.java
@@ -2,64 +2,37 @@ package com.lzn;
 
 /**
  * 整数反转
- *  3ms
  *
  * @author LinZhenNan lin_hehe@qq.com 2019/10/24 23:04
  */
 public class ReverseInteger {
     public int reverse(int x) {
-        // 整数最大值
-        char[] max = Integer.toString(~0 >>> 1).toCharArray();
-        String xStr = Integer.toString(x);
-        char[] chars;
-        boolean isPositive;
-        if ('-' == xStr.charAt(0)) {
-            isPositive = false;
-            chars = xStr.substring(1).toCharArray();
-        } else {
-            isPositive = true;
-            chars = xStr.toCharArray();
-        }
-        StringBuilder sb = new StringBuilder();
-        int last = chars.length - 1;
-        // 尾部是否为零
-        boolean tailZero = true;
-        for (int i = last; i >= 0; i--) {
-            if (tailZero && chars[i] != '0') {
-                tailZero = false;
-            }
-            if (!tailZero) {
-                sb.append(chars[i]);
-            }
-        }
-
-        // 是否溢出
-        chars = sb.toString().toCharArray();
-        // 空字符串返回 0
-        if (0 == sb.length() || chars.length > max.length) {
-            return 0;
-        } else if (chars.length == max.length) {
-            boolean equal = true;
-            for (int i = 0; i < chars.length; i++) {
-                if (chars[i] > max[i]) {
+        // 整型最大值，避免溢出，判断个位以外的所有位
+        int max = (-1 >>> 1) / 10;
+        // 整型最小值，避免溢出，判断个位以外的所有位
+        int min = (1 << 31) / 10;
+        // x 是否为正数
+        boolean isPositive = x > 0 ? true : false;
+        // x 反转的数，reverse 但与方法重名，所以简写
+        int rev = 0;
+        while (x != 0) {
+            // 求个位的数
+            int m = x % 10;
+            // x 去掉个位数
+            x /= 10;
+            // 如果 x 为正数则判断是否溢出，溢出则返回 0
+            if (isPositive){
+                if (rev > max || (rev == max && m > 7)) {
                     return 0;
-                } else if (chars[i] < max[i]) {
-                    equal = false;
-                    break;
                 }
-            }
-            if (equal) {
-                last = max.length - 1;
-                // 判断是否溢出。补码，最小值的绝对值比最大值大一
-                boolean overflow = (isPositive && chars[last] > max[last]) || (!isPositive && chars[last] > max[last] + 1);
-                if (overflow) {
+            } else {
+                // 如果 x 为负数，判断是否溢出，溢出则返回 0
+                if (rev < min || (rev == min && m < -8)) {
                     return 0;
                 }
             }
+            rev = rev * 10 + m;
         }
-        if (!isPositive) {
-            sb.insert(0, '-');
-        }
-        return Integer.parseInt(sb.toString());
+        return rev;
     }
 }
